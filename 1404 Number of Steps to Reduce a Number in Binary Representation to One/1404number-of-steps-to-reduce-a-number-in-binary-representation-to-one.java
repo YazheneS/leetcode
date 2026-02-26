53d0ec5652f35ecf82d89f8a9fc83a9d7895145e
YazheneS <yazh.yazhene@gmail.com>
2026-02-26 18:02:29 +0000

    Code challenge submission for question: 1404 Number of Steps to Reduce a Number in Binary Representation to One

---
+++ 1404 Number of Steps to Reduce a Number in Binary Representation to One/1404number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numSteps(String s) 
+    {
+        int step=0;
+        StringBuilder sb = new StringBuilder(s);  
+        while (!sb.toString().equals("1")) {  
+            if (sb.charAt(sb.length() - 1) == '0')   
+                sb.deleteCharAt(sb.length() - 1);  
+            else 
+            {  
+                int i = sb.length() - 1;  
+                while (i >= 0 && sb.charAt(i) == '1') {  
+                    sb.setCharAt(i, '0');  
+                    i--;  
+                }  
+                if (i >= 0)
+                    sb.setCharAt(i, '1');  
+                else 
+                    sb.insert(0, '1');  
+            }  
+            step++;  
+        }  
+        return step;
+    }
+}
