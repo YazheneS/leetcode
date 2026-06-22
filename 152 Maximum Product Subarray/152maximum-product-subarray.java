27449d2d7d660f3d8c4d623ae54b9f6bc5268606
YazheneS <yazh.yazhene@gmail.com>
2026-06-22 13:50:27 +0000

    Code challenge submission for question: 152 Maximum Product Subarray

---
+++ 152 Maximum Product Subarray/152maximum-product-subarray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int imax = nums[0];
+        int imin = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] < 0) {
+                int temp = imax;
+                imax = imin;
+                imin = temp;
+            }
+
+            imax = Math.max(nums[i], imax * nums[i]);
+            imin = Math.min(nums[i], imin * nums[i]);
+
+            result = Math.max(result, imax);
+        }
+
+        return result;
+    }
+}
