0256e86a54a728e9a3dbcb1f40d05ab6463f0aae
YazheneS <yazh.yazhene@gmail.com>
2026-06-25 12:45:54 +0000

    Code challenge submission for question: 921 Minimum Add to Make Parentheses Valid

---
+++ 921 Minimum Add to Make Parentheses Valid/921minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minAddToMakeValid(String S) {
+        int open = 0, close = 0;
+        for (int i = 0; i < S.length(); ++i) 
+        {
+            if (S.charAt(i) == '(') 
+                open++;
+            else if (open > 0) 
+                open--;
+            else 
+                close++;
+            
+        }
+        return open + close;
+    }
+}
