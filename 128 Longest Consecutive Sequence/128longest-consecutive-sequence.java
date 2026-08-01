8a0c341b19421c715c709075dc67c194d9200d33
YazheneS <yazh.yazhene@gmail.com>
2026-08-01 15:11:50 +0000

    Code challenge submission for question: 128 Longest Consecutive Sequence

---
+++ 128 Longest Consecutive Sequence/128longest-consecutive-sequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums)
+            set.add(i);
+
+        int ans = 0;
+
+        for (int i : set)
+            if (!set.contains(i - 1)) {
+                int len = 1;
+                while (set.contains(i + len))
+                    len++;
+                ans = Math.max(ans, len);
+            }
+
+        return ans;
+    }
+}
