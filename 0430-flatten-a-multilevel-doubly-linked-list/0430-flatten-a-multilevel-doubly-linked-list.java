cfbb31b54b027ece2825ed00fd6a9042c27fa659
Yazhene S <yazh.yazhene@gmail.com>
2026-08-05 23:04:35 +0530

    Code challenge submission: 0430-flatten-a-multilevel-doubly-linked-list

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        if (head == null) return null;
+
+        Stack<Node> stack = new Stack<>();
+        Node curr = head;
+
+        while (curr != null) {
+            if (curr.child != null) {
+               
+                if (curr.next != null) {
+                    stack.push(curr.next);
+                }
+
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+            }
+
+            if (curr.next == null && !stack.isEmpty()) {
+                Node resumed = stack.pop();
+                curr.next = resumed;
+                resumed.prev = curr;
+            }
+
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
