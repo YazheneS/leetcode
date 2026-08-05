e3c3eda9fc755dd35a7c57db596ae7feb6880cef
Yazhene S <yazh.yazhene@gmail.com>
2026-08-05 23:11:59 +0530

    Code challenge submission: 0451-sort-characters-by-frequency

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Character> pq = new PriorityQueue<>(
+            (a, b) -> freq.get(a) != freq.get(b)
+                ? freq.get(b) - freq.get(a)   // higher freq first
+                : a - b                        // lexicographic for ties
+        );
+
+        pq.addAll(freq.keySet());
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            char c = pq.poll();
+            int count = freq.get(c);
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
