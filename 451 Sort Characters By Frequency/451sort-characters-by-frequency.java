bb797d9d11f97b6c969c747209e1902ed64de5b8
Yazhene S <yazh.yazhene@gmail.com>
2026-08-05 17:41:52 +0000

    Code challenge submission for question: 451 Sort Characters By Frequency

--- 451 Sort Characters By Frequency/451sort-characters-by-frequency.java
+++ 451 Sort Characters By Frequency/451sort-characters-by-frequency.java
@@ -1,24 +1,27 @@
 class Solution {
     public String frequencySort(String s) {
         Map<Character, Integer> freq = new HashMap<>();
-        
         for (char c : s.toCharArray()) {
             freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
-        
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>(
-            (a, b) -> b.getValue() - a.getValue()
+
+        PriorityQueue<Character> pq = new PriorityQueue<>(
+            (a, b) -> freq.get(a) != freq.get(b)
+                ? freq.get(b) - freq.get(a)   // higher freq first
+                : a - b                        // lexicographic for ties
         );
-        
-        pq.addAll(freq.entrySet());
-        
-        StringBuilder result = new StringBuilder();
+
+        pq.addAll(freq.keySet());
+
+        StringBuilder sb = new StringBuilder();
         while (!pq.isEmpty()) {
-            Map.Entry<Character, Integer> entry = pq.poll();
-            result.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+            char c = pq.poll();
+            int count = freq.get(c);
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
         }
-        
-        return result.toString();
+
+        return sb.toString();
     }
 }
-
