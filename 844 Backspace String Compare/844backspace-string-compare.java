179a0bed0d22da57ca253b766a6969efdbac2f73
Yazhene S <yazh.yazhene@gmail.com>
2026-08-09 06:05:19 +0000

    Code challenge submission for question: 844 Backspace String Compare

---
+++ 844 Backspace String Compare/844backspace-string-compare.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        int k = processString(sChars);
+        int p = processString(tChars);
+
+        if (k != p) return false;
+
+        for (int i = 0; i < k; i++) {
+            if (sChars[i] != tChars[i]) 
+                return false;
+        }
+
+        return true;
+    }
+
+    private int processString(char[] chars) {
+        int k = 0;
+        for (char c : chars) {
+            if (c != '#') {
+                chars[k++] = c;
+            } 
+            else if (k > 0) {
+                k--;
+            }
+        }
+        return k;
+    }
+}
